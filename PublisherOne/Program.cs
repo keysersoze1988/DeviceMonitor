e2305f684b96f7748d7cefbf9b6ded1347f378fb
Kutlu Ozel <kutlu.ozel.cs@gmail.com>
2021-07-28 00:28:08 +0300

    Add project files.

---
+++ PublisherOne/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace DeviceMonitor
+{
+    class Program
+    {      
+
+        static void Main(string[] args)
+        {
+            MqttClient client;
+            string clientId;
+            
+        // use a ready mqtt broker with no encryption, no authentication, no certificate
+        // no intent to make you install some service :)
+
+            string BrokerAddress = "test.mosquitto.org";
+
+            client = new MqttClient(BrokerAddress);                
+
+            // use a unique id as device name, each time clients are created.
+            clientId = Guid.NewGuid().ToString();
+
+            client.Connect(clientId);
+
+            string topic = "/KutluIot/test";
+            
+            // generate a random number between 1-10
+            Random rnd = new Random();           
+
+            while (true)
+            {
+                Thread.Sleep(2000);
+
+                // generate dummy measurements       
+                string publishMessage = rnd.Next(1, 1000).ToString();
+
+                client.Publish(topic, Encoding.UTF8.GetBytes(publishMessage), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+
+                Console.WriteLine("Message Sent from client : " + client.ClientId + " as : " + publishMessage);
+            }
+
+        }
+
+     
+
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace PublisherTwo
+{
+    class Program
+    {
+        internal static int MessageCount = 0;
+        static void Main(string[] args)
+        {
+
+
+            MqttClient client;
+            string clientId;
+
+            // use a ready mqtt broker with no encryption, no authentication, no certificate
+            // no intent to make you install some service :)
+
+            string BrokerAddress = "test.mosquitto.org";
+
+            client = new MqttClient(BrokerAddress);
+
+            // use a unique id as device name, each time clients are created.
+            clientId = Guid.NewGuid().ToString();
+
+            client.Connect(clientId);
+
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            string topic = "/KutluIot/+";
+            // subscribe to the topic 
+            client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+        }
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+           
+                byte[] buffer = e.Message;
+            
+                string s = "Message Recieved " + System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+               MessageCount++;    
+
+            Console.WriteLine(s);
+            Console.WriteLine("Total Message Count = " + (MessageCount -1) .ToString());
+        }
+    }
+}
